19070f750e35385355401f3e5ad150d9f016b016
rooslucas <rooslucas@live.nl>
2024-12-10 17:12:19 +0100

    Add day 7 part 2

--- day_07/Program.cs
+++ day_07/Program.cs
@@ -18,6 +18,7 @@ public class Program
         string file = "./input/day_07.txt";
         List<Calculation> parsed = Parse(file);
         Part1(parsed);
+        Part2(parsed);
         
     }
 
@@ -57,5 +58,51 @@ public class Program
         }).Sum();
 
         Console.WriteLine(totalCorrect);
+    }
+
+     public static void Part2(List<Calculation> calculations) {
+        long totalCorrect = calculations.Select(x => {
+
+            long options = (long)Math.Pow(3, (x.numbers.Count()-1));
+
+            for (int i = 0; i<options; i++) { 
+                string bin = ConvertBase3(i).PadLeft((x.numbers.Count()-1), '0');
+                // Console.WriteLine(bin);
+                List<string> operators = bin.Select(x => {if (x=='1'){return "+";} else if(x=='0') {return "*";} else {return "||";}}).ToList();
+                long accumulator = x.numbers[0];
+                x.numbers.RemoveAt(0);
+
+                List<(string, long)> opNums = operators.Zip(x.numbers, (l, n) => (l, n)).ToList();
+                opNums.Insert(0, ("+", accumulator));
+                x.numbers.Insert(0, accumulator);
+                long result = opNums.Aggregate( (n1, n2) => {
+                    long acc = 0;
+                    if (n2.Item1 == "+"){ acc = n1.Item2+n2.Item2;} 
+                    else if (n2.Item1 == "*") { acc = n1.Item2*n2.Item2;} 
+                    else { acc = Int64.Parse(n1.Item2.ToString()+n2.Item2.ToString());} return("+", acc);}).Item2;
+                
+                // Console.WriteLine(result);
+                if (result == x.total) {return x.total;}
+            }
+        
+            return 0;
+        }).Sum();
+
+        Console.WriteLine(totalCorrect);
+    }
+
+    public static string ConvertBase3(long number) {
+        if (number == 0)
+            return "0";
+
+        string result = "";
+        while (number > 0)
+        {
+            long remainder = number % 3;
+            result = remainder + result;
+            number /= 3;
+        }
+
+        return result;
     }
 }
