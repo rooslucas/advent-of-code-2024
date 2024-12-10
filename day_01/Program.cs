20caf4c39c738020cb64ce6a95169b2d90551200
rooslucas <rooslucas@live.nl>
2024-12-10 21:01:13 +0100

    Refactor day 1

--- day_01/Program.cs
+++ day_01/Program.cs
@@ -5,7 +5,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string file = "./input/day_01.txt"; 
+        string file = "./input/day_01.txt";
         var (parsed_l, parsed_r) = parse(file);
         int result_1 = solve_1(parsed_l, parsed_r);
         int result_2 = solve_2(parsed_l, parsed_r);
@@ -15,11 +15,11 @@ public class Program
 
     public static (List<int> Left, List<int> Right) parse(string file)
     {
-        string[] lines = File.ReadAllLines(file); 
+        string[] lines = File.ReadAllLines(file);
         List<int> left_list = new List<int>();
         List<int> right_list = new List<int>();
-        
-        foreach(string line in lines)
+
+        foreach (string line in lines)
         {
             List<string> num = line.Split(' ').ToList();
 
@@ -36,29 +36,15 @@ public class Program
 
     }
 
-// part 1 solving
     public static int solve_1(List<int> left, List<int> right)
     {
         left.Sort();
         right.Sort();
-        List<int> result = new List<int>();
-
-        for(int i = 0; i < left.Count; i++){
-
-            result.Add(Math.Abs(left[i] - right[i]));
-        }
-
-        return result.Sum();
+        return Enumerable.Range(0, left.Count()).Select(i => Math.Abs(left[i] - right[i])).Sum();
     }
 
     public static int solve_2(List<int> left, List<int> right)
-    {    
-        List<int> result = new List<int>();
-        for(int i = 0; i < left.Count; i++){
-            int c = right.Where(x => x == left[i]).Count();
-            result.Add(c * left[i]);
-        }
-
-        return result.Sum();
+    {
+        return Enumerable.Range(0, left.Count()).Select(i => right.Where(x => x == left[i]).Count() * left[i]).Sum();
     }
 }
