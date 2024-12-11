8afe851562000d500584877241e07dc92d19f3c3
rooslucas <rooslucas@live.nl>
2024-12-11 16:37:13 +0100

    Add day 11 part 1

---
+++ day_11/Program.cs
@@ -0,0 +1,45 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string file = "./input/day_11.txt";
+        List<string> stones = Parse(file);
+        Part1(stones);
+    }
+
+    public static List<string> Parse(string file)
+    {
+        return File.ReadAllText(file).Split(' ').ToList();
+    }
+
+    public static void Part1(List<string> stones)
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            // Console.WriteLine("New stones\n");
+
+            int currentLength = stones.Count();
+            for (int x = 0; x < currentLength; x++)
+            {
+                string stone = stones[x];
+                if (Int64.Parse(stone) == 0) { stones[x] = "1"; }
+                else if (stone.Count() % 2 == 0)
+                {
+                    if (stone.Count() > 2)
+                    {
+                        stones.Add(stone[0..(stone.Count() / 2)]);
+                        stones[x] = Int64.Parse(stone[(stone.Count() / 2)..]).ToString();
+                    }
+                    else { stones.Add(stone[0].ToString()); stones[x] = (stone[1].ToString()); }
+                }
+                else { stones[x] = (Int64.Parse(stone) * 2024).ToString(); }
+            }
+
+            // foreach (string stone in stones) { Console.WriteLine(stone); }
+            // Console.WriteLine(stones.Count());
+        }
+
+        Console.WriteLine(stones.Count());
+    }
+}
